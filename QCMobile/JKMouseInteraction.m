12a7abb7660247381a9fcbcc6c3fed34eb44f79e
Joris Kluivers <joris@kluivers.nl>
2013-05-30 22:55:10 +0200

    Mouse interaction now sets outputXDrag and outputYDrag

--- QCMobile/JKMouseInteraction.m
+++ QCMobile/JKMouseInteraction.m
@@ -9,13 +9,19 @@
 #import "JKMouseInteraction.h"
 
 #import "JKContext.h"
+#import "JKContextUtil.h"
 
 @interface JKMouseInteraction ()
 @property(nonatomic, strong) NSNumber *outputMouseDown;
 @property(nonatomic, strong) NSNumber *outputClickCount;
+@property(nonatomic, strong) NSNumber *outputXDrag;
+@property(nonatomic, strong) NSNumber *outputYDrag;
 @end
 
-@implementation JKMouseInteraction
+@implementation JKMouseInteraction {
+    BOOL touching;
+    CGPoint touchStart;
+}
 
 @dynamic outputMouseDown, outputClickCount, outputXDrag, outputYDrag;
 
@@ -23,7 +29,26 @@
 {
     UITouch *touch = [context.touches anyObject];
     
+    if (!touch) {
+        touching = NO;
+    }
+    
     // TODO: only update if needed
+    
+    if (touching) {
+        // TODO: proper 'API' access to view, or location in view
+        
+        CGPoint currLocation = [touch locationInView:(UIView *)context];
+        
+        self.outputXDrag = @(JKPixelsToUnits(context, currLocation.x - touchStart.x));
+        self.outputYDrag = @(JKPixelsToUnits(context, currLocation.y - touchStart.y));
+    } else if (touch) {
+        touching = YES;
+        touchStart = [touch locationInView:(UIView *)context];
+        
+        self.outputXDrag = @0;
+        self.outputYDrag = @0;
+    }
     
     self.outputMouseDown = @((touch != nil));
     self.outputClickCount = @(touch.tapCount);
