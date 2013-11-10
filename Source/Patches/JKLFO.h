9d2c5025207a1b28b06e2dcf2d01324737cbbabb
Joris Kluivers <joris@kluivers.nl>
2013-11-10 22:40:28 +0100

    LFO patch, implements all types except PMW & random at the moment.

---
+++ Source/Patches/JKLFO.h
@@ -0,0 +1,38 @@
+//
+//  JKLFO.h
+//  QCDemos
+//
+//  Created by Joris Kluivers on 10/11/13.
+//  Copyright (c) 2013 Joris Kluivers. All rights reserved.
+//
+
+#import "JKPatch.h"
+
+typedef NS_ENUM(NSUInteger, JKLFOType) {
+    JKLFOSine,
+    JKLFOCos,
+    JKLFOTriangle,
+    JKLFOSquare,
+    JKLFOSawtoothUp,
+    JKLFOSawtootDown,
+    JKLFOPMW,
+    JKLFORandom
+} ;
+
+@interface JKLFO : JKPatch
+
+@property(nonatomic, strong) NSNumber *inputOffset;
+
+/*!
+ * Integer input value of type `JKLFOType`
+ */
+@property(nonatomic, strong) NSNumber *inputType;
+
+@property(nonatomic, strong) NSNumber *inputPMWRatio;
+@property(nonatomic, strong) NSNumber *inputPhase;
+@property(nonatomic, strong) NSNumber *inputAmplitude;
+@property(nonatomic, strong) NSNumber *inputPeriod;
+
+@property(nonatomic, readonly) NSNumber *outputValue;
+
+@end
---
+++ Source/Patches/JKLFO.m
@@ -0,0 +1,58 @@
+//
+//  JKLFO.m
+//  QCDemos
+//
+//  Created by Joris Kluivers on 10/11/13.
+//  Copyright (c) 2013 Joris Kluivers. All rights reserved.
+//
+
+#import "JKLFO.h"
+
+@interface JKLFO ()
+@property(nonatomic, strong) NSNumber *outputValue;
+@end
+
+@implementation JKLFO
+
+@dynamic inputType, inputAmplitude, inputOffset, inputPeriod, inputPhase, inputPMWRatio;
+@dynamic outputValue;
+
+- (void) execute:(id<JKContext>)context atTime:(NSTimeInterval)time
+{
+    CGFloat amplitude = [self.inputAmplitude floatValue];
+    CGFloat period = [self.inputPeriod floatValue];
+    CGFloat offset = [self.inputOffset floatValue];
+    
+    CGFloat result;
+    
+    switch ([self.inputType unsignedIntegerValue]) {
+        case JKLFOSine:
+            result = offset + amplitude * sinf(((2*M_PI) / period) * time);
+            break;
+        case JKLFOCos:
+            result = offset + amplitude * cosf(((2*M_PI) / period) * time);
+            break;
+        case JKLFOTriangle:
+            result = offset + amplitude * (fabs(1 - 0.5 * period + fmodf(1-time, period)) - 1);
+            break;
+        case JKLFOSquare:
+            result = offset + amplitude * (-1 + roundf(fmodf(time, period) / period) * 2);
+            break;
+        case JKLFOSawtoothUp:
+            result = offset - amplitude + 2*amplitude * (1/period) * fmodf(time, period);
+            break;
+        case JKLFOSawtootDown:
+            result = offset + amplitude - 2*amplitude * (1/period) * fmodf(time, period);
+            break;
+        case JKLFOPMW:
+            // TODO: implement pmw
+        case JKLFORandom:
+            // TODO: implement random function
+        default:
+            result = offset;
+    }
+    
+    self.outputValue = @(result);
+}
+
+@end
